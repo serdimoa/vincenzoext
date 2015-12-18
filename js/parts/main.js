dd8b343d1278836b942d00789b9d0729f89f0ff7
serdimoa <serdimoa@gmail.com>
2015-12-18 19:57:46 +0500

    yu

--- js/parts/main.js
+++ js/parts/main.js
@@ -374,102 +374,13 @@ $(function() {
         if(data_items['item_category']=="Вторая"){// todo: set name
             dataTable.row.add([
                 "<h3>"+ data_items['item_name']+"</h3><small>"+data_items['item_component']+"</small>",
-                "<select name='fancySelect' class='makeMeFancy'> <option value='0' selected='selected' data-skip='1'>Choose Your Product</option>        	<option value='1' data-icon='img/products/iphone.png' data-html-text='iPhone 4&lt;i&gt;in stock&lt;/i&gt;'>iPhone 4</option>        	<option value='2' data-icon='img/products/ipod.png' data-html-text='iPod &lt;i&gt;in stock&lt;/i&gt;'>iPod</option>            <option value='3' data-icon='img/products/air.png' data-html-text='MacBook Air&lt;i&gt;out of stock&lt;/i&gt;'>MacBook Air</option>            <option value='4' data-icon='img/products/imac.png' data-html-text='iMac Station&lt;i&gt;in stock&lt;/i&gt;'>iMac Station</option>        </select>",
+                "<select class='basic'><option value=''>Select something…</option>  <option>Lorem</option>  <option>Ipsum</option>  <option>Dolor</option>  <option>Sit</option>  <option>Amet</option></select>",
                 "<input type='number' value='1' data-price='"+data_items['item_price']+"' min='1' max='999' class='form-control' aria-label='Text input with multiple buttons'>",
                 "<span class='cena'>"+data_items['item_price']+" <i class='fa fa-rub'></i></span>",
                 "<a href='#0' id='"+data_items['item_id']+"' class='delete'><i class='fa fa-times'></i></a>"
             ]).draw( false );
-            var select = $('select.makeMeFancy');
+            $('.basic').fancySelect();
 
-	        var selectBoxContainer = $('<div>',{
-	        	width		: select.outerWidth(),
-	        	className	: 'tzSelect',
-	        	html		: '<div class="selectBox"></div>'
-	        });
-
-	        var dropDown = $('<ul>',{className:'dropDown'});
-	        var selectBox = selectBoxContainer.find('.selectBox');
-
-	        // Looping though the options of the original select element
-
-	        select.find('option').each(function(i){
-	        	var option = $(this);
-
-	        	if(i==select.attr('selectedIndex')){
-	        		selectBox.html(option.text());
-	        	}
-
-	        	// As of jQuery 1.4.3 we can access HTML5
-	        	// data attributes with the data() method.
-
-	        	if(option.data('skip')){
-	        		return true;
-	        	}
-
-	        	// Creating a dropdown item according to the
-	        	// data-icon and data-html-text HTML5 attributes:
-
-	        	var li = $('<li>',{
-	        		html:	'<img src="'+option.data('icon')+'" /><span>'+
-	        				option.data('html-text')+'</span>'
-	        	});
-
-	        	li.click(function(){
-
-	        		selectBox.html(option.text());
-	        		dropDown.trigger('hide');
-
-	        		// When a click occurs, we are also reflecting
-	        		// the change on the original select element:
-	        		select.val(option.val());
-
-	        		return false;
-	        	});
-
-	        	dropDown.append(li);
-	        });
-
-	        selectBoxContainer.append(dropDown.hide());
-	        select.hide().after(selectBoxContainer);
-
-	        // Binding custom show and hide events on the dropDown:
-
-	        dropDown.bind('show',function(){
-
-	        	if(dropDown.is(':animated')){
-	        		return false;
-	        	}
-
-	        	selectBox.addClass('expanded');
-	        	dropDown.slideDown();
-
-	        }).bind('hide',function(){
-
-	        	if(dropDown.is(':animated')){
-	        		return false;
-	        	}
-
-	        	selectBox.removeClass('expanded');
-	        	dropDown.slideUp();
-
-	        }).bind('toggle',function(){
-	        	if(selectBox.hasClass('expanded')){
-	        		dropDown.trigger('hide');
-	        	}
-	        	else dropDown.trigger('show');
-	        });
-
-	        selectBox.click(function(){
-	        	dropDown.trigger('toggle');
-	        	return false;
-	        });
-
-	        // If we click anywhere on the page, while the
-	        // dropdown is shown, it is going to be hidden:
-
-	        $(document).click(function(){
-	        	dropDown.trigger('hide');
-	        });
         }
         else{
              dataTable.row.add([
