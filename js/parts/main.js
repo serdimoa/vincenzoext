6866422f70d3480eaf51f06d662f1952c78dd0a8
serdimoa <serdimoa@gmail.com>
2016-01-25 12:01:18 +0500

    edit

--- js/parts/main.js
+++ js/parts/main.js
@@ -808,7 +808,22 @@ $(".logoa").click(function (e) {
     function init() {
         // preload images
         console.log(global_sale);
+        jQuery('#scrollup img').mouseover( function(){
+	    	jQuery( this ).animate({opacity: 0.65},100);
+	    }).mouseout( function(){
+	    	jQuery( this ).animate({opacity: 1},100);
+	    }).click( function(){
+            $('html,body').animate({ scrollTop: 0 }, 'slow');
+	    	return false;
+	    });
 
+	    jQuery(window).scroll(function(){
+	    	if ( jQuery(document).scrollTop() > 0 ) {
+	    		jQuery('#scrollup').fadeIn('fast');
+	    	} else {
+	    		jQuery('#scrollup').fadeOut('fast');
+	    	}
+	    });
         if ($('.index_page').length) {
             initIsotope();
             initEvents();
